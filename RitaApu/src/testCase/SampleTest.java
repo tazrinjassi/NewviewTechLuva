c65fce25edb356610e29601636fd6bf3112950e7
jassitazrin <jassitazrin@gmail.com>
2022-07-07 20:06:12 -0400

    I want to add

---
+++ RitaApu/src/testCase/SampleTest.java
@@ -0,0 +1,29 @@
+package testCase;
+
+public class SampleTest {
+	int age = 65;
+
+
+	public static void main(String[] args) {
+	SampleTest NahidClass = new SampleTest();
+    NahidClass.hhh();
+	}
+public void hhh() {
+	if (age>0 && age <30) {
+		if(age<31 && age >60) {
+			System.out.println(" they are not teenager");	
+		}
+		else {
+			System.out.println("you are young");
+		}
+	}
+
+
+	else {
+		System.out.println("wrong input");
+	}
+
+	
+	
+	
+}}
